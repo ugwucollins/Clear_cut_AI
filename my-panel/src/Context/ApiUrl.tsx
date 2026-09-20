79da4086272561b576d6a56d136a45abb58bb526
Ugwu collins <ugwucollins027@gmail.com>
2026-09-20 13:58:17 +0100

    adding the api path

--- my-panel/src/Context/ApiUrl.tsx
+++ my-panel/src/Context/ApiUrl.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BaseUrl = import.meta.env.VITE_BASEURL_API_PATH;
+const BaseUrl = import.meta.env.BASEURL_API || import.meta.env.VITE_BASEURL_API_PATH;
+// const BaseUrl = import.meta.env.VITE_BASEURL_API_PATH;
 
 export const ApiUrl = axios.create({
   baseURL: BaseUrl,
